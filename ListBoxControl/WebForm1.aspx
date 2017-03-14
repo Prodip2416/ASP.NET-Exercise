c11f6a1c9974593d8ed0219f731f34ac11dc7bd9
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-15 00:20:50 +0600

    ASP.NET

---
+++ ListBoxControl/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ListBoxControl.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ListBox ID="ListBox1" runat="server" SelectionMode="Single">
+           <asp:ListItem Text="Diploma" Value="1"></asp:ListItem>
+           <asp:ListItem Text="Graduate" Value="2"></asp:ListItem>
+           <asp:ListItem Text="Post Graduate" Value="3"></asp:ListItem>
+           <asp:ListItem Text="Doctrate" Value="4"></asp:ListItem>
+        </asp:ListBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ListBoxControl/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ListBoxControl
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //if (ListBox1.SelectedIndex != -1)
+            //{
+            //    Response.Write(ListBox1.SelectedItem.Text + "<br/>");
+            //    Response.Write(ListBox1.SelectedItem.Value + "<br/>");
+            //    Response.Write(ListBox1.SelectedIndex.ToString() + "<br/>");
+            //}
+
+            foreach(ListItem li in ListBox1.Items)
+            {
+                if (li.Selected)
+                {
+                    Response.Write("Text = " + li.Text + "<br/>");
+                    Response.Write("Value = " + li.Value + "<br/>");
+                    Response.Write("Index = " + ListBox1.SelectedIndex.ToString() + "<br/>");
+                    Response.Write("-----------------------<br/>");
+                }
+            }
+        }
+    }
+}
