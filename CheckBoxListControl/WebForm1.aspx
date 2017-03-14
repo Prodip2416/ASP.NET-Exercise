41acabfed0beb2db35926144adb9c5b248d7bf02
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-15 00:13:25 +0600

    ASP.NET

---
+++ CheckBoxListControl/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CheckBoxListControl.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server" RepeatDirection="Horizontal">
+            <asp:ListItem Text="Diploma" Value="1"></asp:ListItem>
+            <asp:ListItem Text="Graduate" Value="2"></asp:ListItem>
+            <asp:ListItem Text="Post Graduate" Value="3"></asp:ListItem>
+            <asp:ListItem Text="Doctrate" Value="4"></asp:ListItem>
+        </asp:CheckBoxList>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ CheckBoxListControl/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CheckBoxListControl
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write(CheckBoxList1.SelectedIndex.ToString());
+            Response.Write("<br/>");
+
+            if (CheckBoxList1.SelectedItem != null)
+            {
+                Response.Write(CheckBoxList1.SelectedItem.Text);
+                Response.Write("<br/>");
+                Response.Write(CheckBoxList1.SelectedItem.Value);
+            }
+
+
+            //    foreach(ListItem li in CheckBoxList1.Items)
+            //    {
+            //        if (li.Selected)
+            //        {
+            //            Response.Write("Text = " + li.Text+", ");
+            //            Response.Write("Value = " + li.Value + ", ");
+            //            Response.Write("Index = " + CheckBoxList1.Items.IndexOf(li) + ", ");
+            //            Response.Write("<br/>");
+            //        }
+            //    }
+        }
+    }
+}
