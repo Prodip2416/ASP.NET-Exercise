88fe5bf24975ddb103423d815a8b9ce0f7332b1a
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-15 00:39:19 +0600

    ASP.NET

---
+++ All_List_Control/WebForm1.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="All_List_Control.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+            </asp:CheckBoxList>
+            <br />
+            <br />
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+            </asp:RadioButtonList>
+            <br />
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server" SelectionMode="Multiple"></asp:ListBox>
+            <br />
+            <br />
+            <asp:BulletedList ID="BulletedList1" runat="server">
+            </asp:BulletedList>
+            <br/>
+            <br/>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ All_List_Control/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace All_List_Control
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                PopulatedItem(DropDownList1);
+                PopulatedItem(CheckBoxList1);
+                PopulatedItem(RadioButtonList1);
+                PopulatedItem(ListBox1);
+                PopulatedItem(BulletedList1);
+            }
+        }
+        private void PopulatedItem(ListControl listitem)
+        {
+            ListItem li1 = new ListItem("Item1", "1");
+            ListItem li2 = new ListItem("Item2", "2");
+            ListItem li3 = new ListItem("Item3", "3");
+
+            listitem.Items.Add(li1);
+            listitem.Items.Add(li2);
+            listitem.Items.Add(li3);
+        }
+        private void GetMultipleSelection(ListControl listControl)
+        {
+            foreach(ListItem li in listControl.Items)
+            {
+                if (li.Selected)
+                {
+                    Response.Write("Text = " + li.Text + " Value = " + li.Value + " Index = " + listControl.Items.IndexOf(li).ToString()+"<br/>");
+                }
+            }
+        }
+        private void GetSingleSelection(ListControl listControl)
+        {
+            if (listControl.SelectedIndex != -1)
+            {
+                Response.Write("Text = " + listControl.SelectedItem.Text + "<br/>");
+                Response.Write("Value = " + listControl.SelectedItem.Value + "<br/>");
+                Response.Write("Index = " + listControl.SelectedIndex.ToString() + "<br/>");
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            GetMultipleSelection(ListBox1);
+          //  GetSingleSelection(DropDownList1);
+            //GetSingleSelection(CheckBoxList1);
+            //GetSingleSelection(RadioButtonList1);
+            //GetSingleSelection(BulletedList1);
+        }
+    }
+}
