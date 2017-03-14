904d145098140724f9d6daa51b4d1294cb3be44e
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-14 23:53:39 +0600

    ASP.NET

---
+++ DataBindingToXMLFile/WebForm1.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DataBindingToXMLFile.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ DataBindingToXMLFile/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace DataBindingToXMLFile
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            string path = Server.MapPath("Countries.xml");
+            ds.ReadXml(path);
+            DropDownList1.DataTextField = "CountryName";
+            DropDownList1.DataValueField = "CountryId";
+            DropDownList1.DataSource=ds;
+            DropDownList1.DataBind();
+
+            ListItem li = new ListItem("Select", "-1");
+            DropDownList1.Items.Insert(0, li);
+
+        }
+    }
+}
