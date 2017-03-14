bc4d133d5587ff5ae8bc5427bbd4223d91dde7a0
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-14 23:33:36 +0600

    ASP.NET

---
+++ ChechBoxControll/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ChechBoxControll.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <fieldset style="width:350px">
+        <legend><b>Education</b></legend>
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="Graduate"/>
+        <asp:CheckBox ID="CheckBox2" runat="server" Text="Post Graduate"/>
+        <asp:CheckBox ID="CheckBox3" runat="server" Text="Doctrate"/>
+    </fieldset>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChechBoxControll/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace ChechBoxControll
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbUserChoice = new StringBuilder();
+
+            if(CheckBox1.Checked)
+            {
+                sbUserChoice.Append(CheckBox1.Text);
+            }
+            if (CheckBox2.Checked)
+            {
+                sbUserChoice.Append(", "+CheckBox2.Text);
+            }
+            if (CheckBox3.Checked)
+            {
+                sbUserChoice.Append(", "+CheckBox3.Text);
+            }
+
+            Response.Write("Your Selection " + sbUserChoice.ToString());
+        }
+    }
+}
