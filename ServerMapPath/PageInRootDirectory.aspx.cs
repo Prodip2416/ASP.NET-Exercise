22f7beea1cf69db58d8dc274d2fe1ade2d8dc869
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-14 23:57:23 +0600

    ASP.NET

---
+++ ServerMapPath/PageInRootDirectory.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerMapPath
+{
+    public partial class PageInRootDirectory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(Server.MapPath(".") + "<br/>");
+            //Response.Write(Server.MapPath("..") + "<br/>");
+            Response.Write(Server.MapPath("~") + "<br/>");
+           // Response.Write(Server.MapPath("../..") + "<br/>");
+        }
+    }
+}
