2c114e17c9d8c862764ae28901c95da0b4815fc5
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-14 23:49:04 +0600

    ASP.NET

---
+++ DataBinding_DropdownList/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DataBinding_DropdownList.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownList1" DataTextField = "CityName"
+             DataValueField = "CityId" runat="server">
+
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ DataBinding_DropdownList/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataBinding_DropdownList
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string str = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(str))
+                {
+                    SqlCommand cmd = new SqlCommand("Select CityId,Cityname,CountryName from tblCity", con);
+                    con.Open();
+                    DropDownList1.DataSource = cmd.ExecuteReader();
+                    DropDownList1.DataBind();
+                }
+            }
+        }
+    }
+}
