8b5e6a3a9e1aabefe7b6df5704cbfb410956bd37
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-14 23:42:43 +0600

    ASP.NET

---
+++ CommandEvent/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CommandEvent.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Button ID="printButton" runat="server" Text="Print" OnCommand="CommandButton_Click" CommandName="Print" />
+    <asp:Button ID="deleteButton" runat="server" Text="Delete" OnCommand="CommandButton_Click" CommandName="Delete" />
+    <asp:Button ID="topButton" runat="server" Text="Show Top 10 Employees" OnCommand="CommandButton_Click" CommandName="Show"
+        CommandArgument="Top10" />
+    <asp:Button ID="bottomButton" runat="server" Text="Show Bottom 10 Employees" OnCommand="CommandButton_Click" CommandName="Show"
+        CommandArgument="Bottom10" />
+
+
+        <asp:Label ID="lebel" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CommandEvent/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CommandEvent
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void CommandButton_Click(object sender,CommandEventArgs e)
+        {
+            switch (e.CommandName)
+            {
+                case "Print":
+                    lebel.Text = "You click print button";
+                    break;
+                case "Delete":
+                    lebel.Text = "You click delete button";
+                    break;
+                case "Show":
+                    if(e.CommandName.ToString()=="Top10")
+                    {
+                        lebel.Text = "You click show top 10 employee";
+                    }
+                    else
+                    {
+                        lebel.Text = "You click show Bottom 10 employee";
+                    }
+                    break;
+                default:
+                    lebel.Text = "We do not know which button you click";
+                    break;
+            }
+        }
+    }
+}
