83cb91b7cf6f2c3fe0ae21f76b6ed201282728a4
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-15 00:44:32 +0600

    ASP.NET

---
+++ FileUpload/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FilrUpload.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        &nbsp;&nbsp;
+        <asp:Button ID="btnUpload" runat="server" Text="Upload File" OnClick="btnUpload_Click" />
+        <br/><br/>
+        <asp:Label ID="lblMessage" runat="server" font-bold="true"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ FileUpload/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FilrUpload
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fileExtension =System.IO.Path.GetExtension(FileUpload1.FileName);
+                if (fileExtension.ToLower() != ".doc" && fileExtension.ToLower() != ".docx")
+                {
+                    lblMessage.Text = "Please select a .doc or .docx file";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    int fileSize = FileUpload1.PostedFile.ContentLength;
+                    if (fileSize > 2097152)
+                    {
+                        lblMessage.Text = "Maximum file size (2MB) exceeded.";
+                        lblMessage.ForeColor = System.Drawing.Color.Green;
+                    }
+                    else
+                    {
+
+                        FileUpload1.SaveAs(Server.MapPath("~/Uploads/" + FileUpload1.FileName));
+                        lblMessage.Text = "File Uploaded";
+                        lblMessage.ForeColor = System.Drawing.Color.Green;
+                    }
+                }
+            }
+            else
+            {
+                lblMessage.Text = "Please select a file";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
