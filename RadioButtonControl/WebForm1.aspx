d951916b73e7042d427f5083c04288bbd472f9ff
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-14 23:30:07 +0600

    ASP.NET

---
+++ RadioButtonControl/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RadioButtonControl.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <fieldset>
+        <legend><b>Gender</b></legend>
+        <asp:RadioButton ID="RadioButton1" runat="server" Text="Male" GroupName="GroupName" OnCheckedChanged="RadioButton1_CheckedChanged" />
+        <asp:RadioButton ID="RadioButton2" runat="server" text="Female" GroupName="GroupName"/>
+        <asp:RadioButton ID="RadioButton3" runat="server" Text="Unknown" GroupName="GroupName"/>
+    </fieldset>
+
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ RadioButtonControl/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RadioButtonControl
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(RadioButton1.Checked)
+            {
+                Response.Write("Your Gender Is : " + RadioButton1.Text + "<br/>");
+            }
+
+            if (RadioButton2.Checked)
+            {
+                Response.Write("Your Gender Is : " + RadioButton2.Text + "<br/>");
+            }
+
+            if (RadioButton3.Checked)
+            {
+                Response.Write("Your Gender Is : " + RadioButton3.Text + "<br/>");
+            }
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Response.Write("Male Radio Button Checked <br/>");
+        }
+    }
+}
