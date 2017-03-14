12746a00bf81566681e08a9d0eec84ac02888a3a
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-03-15 00:16:42 +0600

    ASP.NET

---
+++ CheckBoxList_Select_DeSelect/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CheckBoxList_Select_DeSelect.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="Button2" runat="server" Text="Select All" OnClick="Button2_Click" />
+        &nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" Text="DeSelect All" OnClick="Button3_Click" />
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server" RepeatDirection="Horizontal">
+             <asp:ListItem Text="Diploma" Value="1"></asp:ListItem>
+            <asp:ListItem Text="Graduate" Value="2"></asp:ListItem>
+            <asp:ListItem Text="Post Graduate" Value="3"></asp:ListItem>
+            <asp:ListItem Text="Doctrate" Value="4"></asp:ListItem>
+        </asp:CheckBoxList>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ CheckBoxList_Select_DeSelect/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CheckBoxList_Select_DeSelect
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CheckBoxList1.SelectedIndex = 2;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            foreach (ListItem li in CheckBoxList1.Items)
+            {
+                if (li.Selected)
+                {
+                    Response.Write(li.Text+"<br/>");
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+           foreach(ListItem li in CheckBoxList1.Items)
+            {
+               li.Selected = true;
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            foreach (ListItem li in CheckBoxList1.Items)
+            {
+                li.Selected = false;
+            }
+        }
+    }
+}
