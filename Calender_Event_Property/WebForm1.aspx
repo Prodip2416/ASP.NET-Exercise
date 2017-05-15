8ac43e6d4600bd26dc60966010f7f9534625870c
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-05-15 13:27:00 +0600

    ASP.NET

---
+++ Calender_Event_Property/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Calender_Event_Property.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Calendar ID="Calendar1" runat="server" Caption="Training Calender" CaptionAlign="Top" DayNameFormat="Full" NextMonthText="&gt;&gt;" NextPrevFormat="ShortMonth" OnDayRender="Calendar1_DayRender" OnSelectionChanged="Calendar1_SelectionChanged" PrevMonthText="&lt;&lt;" SelectionMode="DayWeek">
+            <DayHeaderStyle BackColor="#CC3300" ForeColor="White" />
+        </asp:Calendar>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Calender_Event_Property/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Calender_Event_Property
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            foreach(DateTime selectedData in Calendar1.SelectedDates)
+            {
+                Response.Write(selectedData.ToShortDateString() + "<br/>");
+            }
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if(!e.Day.IsOtherMonth && e.Day.Date.Day % 2 == 0)
+             {
+                e.Cell.BackColor = System.Drawing.Color.Red;
+                e.Cell.ForeColor = System.Drawing.Color.White;
+                e.Cell.Font.Bold = true;
+                e.Cell.Text = "x";
+                e.Cell.ToolTip = "Blocked";
+            }
+            else
+            {
+                e.Cell.ToolTip = "Available";
+            }
+        }
+    }
+}
