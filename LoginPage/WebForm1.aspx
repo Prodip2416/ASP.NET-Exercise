5ad85c91b7588c7beda09c336f8e6c01bf0b452f
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-05-23 00:46:03 +0600

    ASP.NET

---
+++ LoginPage/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LoginPage.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+        <tr>
+           <td>
+               <b>Name:</b>
+           </td>
+            <td>
+                <asp:TextBox ID="txtName" Width="200px" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+           <tr>
+           <td>
+               <b>Password:</b>
+           </td>
+            <td>
+                <asp:TextBox ID="txtPassword" Width="200px" runat="server" TextMode="Password"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:Button ID="btnSubmit" runat="server" Text="Sign in" OnClick="btnSubmit_Click" /></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label></td>
+        </tr>
+    </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ LoginPage/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginPage
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con= new SqlConnection(CS);
+            string query = "select count(*) from Login where Name= '"+txtName.Text+"' and Password= '"+txtPassword.Text+"'";
+            SqlCommand cmd= new SqlCommand(query,con);
+            con.Open();
+            int temp =Convert.ToInt32(cmd.ExecuteScalar().ToString());
+            con.Close();
+            if (temp==1)
+            {
+                Session["UserName"] = txtName.Text;
+                Response.Redirect("~/WebForm2.aspx");
+            }
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text="Invalid Name or Password.";
+            }
+
+        }
+    }
+}
