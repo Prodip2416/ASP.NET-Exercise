2e3b0c50c12f2e49c71fd0ad2190f90b3df88286
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-05-27 23:15:16 +0600

    ASP.NET

---
+++ Cookieless_SessionState/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cookieless_SessionState
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNextPage_Click(object sender, EventArgs e)
+        {
+            Session["Name"] = txtName.Text;
+            Session["Email"] = txtEmail.Text;
+
+            Response.Redirect("~/WebForm2.aspx");
+        }
+    }
+}
---
+++ Cookieless_SessionState/WebForm2.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cookieless_SessionState
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Name"] != null)
+            {
+                lblName.Text = Session["Name"].ToString();
+            }
+            if (Session["Email"] != null)
+            {
+                lblEmail.Text = Session["Email"].ToString();
+            }
+        }
+    }
+}
