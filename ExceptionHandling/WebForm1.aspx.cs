a6083b1a956d7aad3fab17e54aefaac89f9a08fc
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-05-31 01:52:10 +0600

    ASP.NET

---
+++ ExceptionHandling/WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExceptionHandling
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          //  Response.Write(System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+
+            try
+            {
+                DataSet ds = new DataSet();
+
+                ds.ReadXml(Server.MapPath("~/countries.xml"));
+
+                grid.DataSource = ds;
+                grid.DataBind();
+            }
+            catch (System.IO.FileNotFoundException fileNotFoundException)
+            {
+                lblMessage.Text = "file is missing.";
+            }
+            catch (System.UnauthorizedAccessException unauthorizedAccessException)
+            {
+                lblMessage.Text = "Access not denie";
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "There is unknown problem";
+            }
+            finally
+            {
+                //Clean up code
+            }
+          
+        }
+    }
+}
