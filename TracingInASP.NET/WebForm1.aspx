d6da8c2c8c6d2f8f418fafa03f0fd9894e65e678
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-06-04 12:28:40 +0600

    Add files via upload

---
+++ TracingInASP.NET/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" Trace="true"  AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TracingInASP.NET.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server">
+            
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TracingInASP.NET/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TracingInASP.NET
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+              DataSet ds = new DataSet();
+                ds.ReadXml(Server.MapPath("~/XMLFile1.xml"));
+
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+    }
+}
