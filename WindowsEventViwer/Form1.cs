94d7eda000d23233bd88c6e28691b5e0b7738efc
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-06-04 12:18:56 +0600

    ASP.NET

---
+++ WindowsEventViwer/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsEventViwer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty || textBox2.Text != String.Empty)
+            {
+                System.Diagnostics.EventLog.CreateEventSource(textBox1.Text, textBox2.Text);
+                MessageBox.Show("Event Log are created.");
+            }
+            else
+            {
+                MessageBox.Show("Event log are Required.");
+            }
+        }
+    }
+}
