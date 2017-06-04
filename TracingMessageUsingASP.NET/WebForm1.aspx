abdb8341a72a747ae71ecb7fef94ea8de1bda061
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-06-04 12:34:35 +0600

    ASP.NET

---
+++ TracingMessageUsingASP.NET/WebForm1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" Trace="true" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TracingMessageUsingASP.NET.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+ <div style="font-family: Arial">
+    <table style="border: 1px solid black">
+        <tr>
+            <td>
+                <b>First Number</b>
+            </td>
+            <td>
+                <asp:TextBox ID="txtFirstNumber" runat="server">
+                </asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <b>Second Number</b>
+            </td>
+            <td>
+                <asp:TextBox ID="txtSecondNumber" runat="server">
+                </asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:Button ID="btnDivide" runat="server" Text="Divide" 
+                OnClick="btnDivide_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:Label ID="lblMessage" runat="server" Font-Bold="true">
+                </asp:Label>
+            </td>
+        </tr>
+    </table>
+</div> 
+    </form>
+</body>
+</html>
---
+++ TracingMessageUsingASP.NET/WebForm1.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TracingMessageUsingASP.NET
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDivide_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int FirstNumber = Convert.ToInt32(txtFirstNumber.Text);
+                int SecondNumber = Convert.ToInt32(txtSecondNumber.Text);
+
+                lblMessage.ForeColor = System.Drawing.Color.Navy;
+                int Result = FirstNumber / SecondNumber;
+                lblMessage.Text = Result.ToString();
+            }
+            catch (FormatException formatException)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Only numbers are allowed";
+                // Check if tracing is enabled
+                if (Trace.IsEnabled)
+                {
+                    // Write the exception message to the trace log
+                    Trace.Write(formatException.Message);
+                }
+            }
+            catch (OverflowException overflowException)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Numbers must be between " + Int32.MinValue.ToString() + " and " + Int32.MaxValue.ToString();
+                if (Trace.IsEnabled)
+                {
+                    Trace.Warn(overflowException.Message);
+                }
+            }
+            catch (DivideByZeroException divideByZeroException)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Denominator cannot be ZERO";
+                if (Trace.IsEnabled)
+                {
+                    Trace.Warn(divideByZeroException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "An unknown problem has occured. Please try later";
+                if (Trace.IsEnabled)
+                {
+                    Trace.Warn(exception.Message);
+                }
+            }
+        }
+    }
+}
