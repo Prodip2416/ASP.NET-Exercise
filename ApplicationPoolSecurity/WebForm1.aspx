3d760caf8954f3afd7f0e1deaa17c046dbb6fcb6
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-06-17 08:40:23 +0600

    ASP.NET

---
+++ ApplicationPoolSecurity/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ApplicationPoolSecurity.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:GridView ID="gridView" runat="server"></asp:GridView><br/>
+        <asp:FileUpload ID="fileUpload" runat="server"/>
+       &nbsp; <asp:Button ID="Button1" runat="server" Text="Read File" OnClick="Button1_Click" />
+        <br/>
+        <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ApplicationPoolSecurity/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ApplicationPoolSecurity
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Identity User = "+ 
+                System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (fileUpload.HasFile)
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml("C:\\Data\\" + fileUpload.FileName);
+                gridView.DataSource = ds;
+                gridView.DataBind();
+                lblMessage.Text=String.Empty;
+            }
+            else
+            {
+                lblMessage.Text = "Please Select a File.";
+            }
+        }
+    }
+}
