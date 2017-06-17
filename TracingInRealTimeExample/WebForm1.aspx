bcc80c16fa96a179512284e2d933d952cf9dfb90
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-06-17 08:28:39 +0600

    ASP.NET

---
+++ TracingInRealTimeExample/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" Trace="true" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TracingInRealTimeExample.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <b>AllEmployee</b>:
+&nbsp;<asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        <br/>
+        <b>EmployeeByGender:</b>
+&nbsp;<asp:GridView ID="GridView2" runat="server"></asp:GridView>
+        <br/>
+        <b>EmployeeByDepartment</b>:
+&nbsp;<asp:GridView ID="GridView3" runat="server"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TracingInRealTimeExample/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TracingInRealTimeExample
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Trace.Warn("GetAllEmployee() started");
+            GetAllEmployee();
+            Trace.Warn("GetAllEmployee() Complete");
+            Trace.Warn("GetGender() started");
+            GetGender();
+            Trace.Warn("GetGender() Complete");
+            Trace.Warn("GetDepartment() started");
+            GetDepartment();
+            Trace.Warn("GetDepartment() Complete");
+        }
+
+        private void GetAllEmployee()
+        {
+            GridView1.DataSource = ExecuteStoredProcedure("spGetssEmployees");
+            GridView1.DataBind();
+        }
+
+        private void GetGender()
+        {
+            GridView2.DataSource = ExecuteStoredProcedure("spGetEmployeesByGender");
+           GridView2.DataBind();
+        }
+
+        private void GetDepartment()
+        {
+            GridView3.DataSource = ExecuteStoredProcedure("spGetEmployeesByDepartment");
+           GridView3.DataBind();
+        }
+
+        private DataSet ExecuteStoredProcedure(string SPparam )
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con= new SqlConnection(CS);
+            SqlDataAdapter da= new SqlDataAdapter(SPparam,con);
+            da.SelectCommand.CommandType=CommandType.StoredProcedure;
+            DataSet ds= new DataSet();
+            da.Fill(ds);
+
+            return ds;
+        }
+    }
+}
