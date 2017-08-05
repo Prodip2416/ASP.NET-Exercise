ef9a4afe4510adde1c6035081011df131e8d7411
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-08-05 19:40:44 +0600

    Add files via upload

---
+++ CacheingGetPostRequest/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CacheingGetPostRequest.WebForm1" %>
+<%@OutputCache Duration="30" VaryByParam="DroDownList1" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+ <div style="font-family:Arial">
+    Select Product: 
+    <asp:DropDownList ID="DropDownList1" AutoPostBack="true" runat="server" 
+        onselectedindexchanged="DropDownList1_SelectedIndexChanged">
+        <asp:ListItem Text="All" Value="All"></asp:ListItem>
+        <asp:ListItem Text="Laptop" Value="Laptop"></asp:ListItem>
+        <asp:ListItem Text="Iphone" Value="Iphone"></asp:ListItem>
+        <asp:ListItem Text="LCD TV" Value="LCD TV"></asp:ListItem>
+        <asp:ListItem Text="Destop" Value="Destop"></asp:ListItem>
+    </asp:DropDownList>
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+    <br />
+    <br />
+    Server Time: 
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+    <br />
+    <br />
+    Client Time:
+    <script type="text/javascript">
+        document.write(Date());
+    </script>
+</div>
+    </form>
+</body>
+</html>
---
+++ CacheingGetPostRequest/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CacheingGetPostRequest
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetProductByName("All");
+            }
+            Label1.Text = DateTime.Now.ToString();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetProductByName(DropDownList1.SelectedValue);
+        }
+
+        private void GetProductByName(string ProductName)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlDataAdapter da = new SqlDataAdapter("spGetProductsByName", con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter paramProductName = new SqlParameter();
+            paramProductName.ParameterName = "@ProductName";
+            paramProductName.Value = ProductName;
+            da.SelectCommand.Parameters.Add(paramProductName);
+
+            DataSet DS = new DataSet();
+            da.Fill(DS);
+            GridView1.DataSource = DS;
+            GridView1.DataBind();
+        }
+    }
+}
