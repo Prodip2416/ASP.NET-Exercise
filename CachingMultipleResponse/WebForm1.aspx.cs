29f238dfa48a637f48b12db261405d187e0d72cb
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-08-05 19:34:20 +0600

    ASP.NET

---
+++ CachingMultipleResponse/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CachingMultipleResponse
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetProductName("All");
+            }
+            Label1.Text = DateTime.Now.ToString();
+        }
+
+        protected void GetProductName(string ProductName)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con= new SqlConnection(CS);
+            SqlDataAdapter da= new SqlDataAdapter("spGetProductsByName", con);
+            da.SelectCommand.CommandType=CommandType.StoredProcedure;
+
+            SqlParameter parameterName= new SqlParameter();
+            parameterName.ParameterName = "@ProductName";
+            parameterName.Value = ProductName;
+            da.SelectCommand.Parameters.Add(parameterName);
+
+            DataSet ds= new DataSet();
+            da.Fill(ds);
+
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetProductName(DropDownList1.SelectedValue);
+        }
+    }
+}
