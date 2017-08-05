f9b007b9b68265069e196688099c5724bde53f35
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-08-05 19:36:55 +0600

    ASP.NET

---
+++ ControllingCacheingInCode/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ControllingCacheingInCode.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    Server Time:
+        <asp:Label ID="Label" runat="server" Text=""></asp:Label>
+        <br/>
+        <br/>
+        Client Time:
+        <script type="text/javascript">
+            document.write(Date());
+        </script>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ControllingCacheingInCode/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControllingCacheingInCode
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //<% @OutputCache Duration = "30" VaryByParam = "None" Location = "ServerAndClient" %>
+
+            Response.Cache.SetExpires(DateTime.Now.AddSeconds(30));
+            Response.Cache.VaryByParams["None"] = true;
+            Response.Cache.SetCacheability(HttpCacheability.ServerAndPrivate);
+
+            Label.Text = "This page is cached @ " + DateTime.Now.ToString();
+        }
+    }
+}
