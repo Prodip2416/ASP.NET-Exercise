65d5c8b1d605b37508041a82b1a61ad482a6148e
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-08-05 19:32:18 +0600

    ASP.NET

---
+++ CachingInASP/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CachingInASP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+             SqlDataAdapter da = new SqlDataAdapter("spGetProducts",con);
+            da.SelectCommand.CommandType=CommandType.StoredProcedure;
+            DataSet ds= new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+            Label1.Text = DateTime.Now.ToString();
+        }
+    }
+}
