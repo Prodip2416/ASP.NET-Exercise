d4a1e7b900bd0b47639869532d957c26850486d4
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-08-25 10:52:34 +0600

    ASP.NET

---
+++ CacheDependencyOnFiles/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CacheDependencyOnFiles.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="Button1" runat="server" Text="Get Countries" OnClick="Button1_Click" />
+        <br/><br/>
+        <asp:GridView ID="gvCountries" runat="server"></asp:GridView>
+        <br/>
+        <br/>
+        <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CacheDependencyOnFiles/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CacheDependencyOnFiles
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Cache["CountriesData"] != null)
+            {
+                DataSet ds = (DataSet) Cache["CountriesData"];
+                // Set the dataset as the datasource
+                gvCountries.DataSource = ds;
+                gvCountries.DataBind();
+                // Retrieve the total rows count
+                lblMessage.Text = ds.Tables[0].Rows.Count.ToString() + " rows retrieved from cache.";
+            }
+            else
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(Server.MapPath("~/Data/Countrys.xml"));
+                Cache.Insert("CountriesData", ds, null, DateTime.Now.AddSeconds(20), System.Web.Caching.Cache.NoSlidingExpiration);
+                gvCountries.DataSource = ds;
+                gvCountries.DataBind();
+                lblMessage.Text = ds.Tables[0].Rows.Count.ToString() + " rows retrieved from the file.";
+            }
+        }
+    }
+}
