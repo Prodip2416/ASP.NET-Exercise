5d8981d61cafb636187e36e5c84864d90768a64b
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-08-25 11:10:05 +0600

    ASP.NET

---
+++ CacheDependancyOnSQLServer/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CacheDependancyOnSQLServer.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+  <div style="font-family: Arial">
+    <asp:Button ID="btnGetData" runat="server" Text="Get Data" 
+    OnClick="btnGetData_Click" />
+    <br />
+    <br />
+    <asp:GridView ID="gvProducts" runat="server">
+    </asp:GridView>
+    <br />
+    <asp:Label ID="lblStatus" runat="server" Font-Bold="true">
+    </asp:Label>
+</div>
+    </form>
+</body>
+</html>
---
+++ CacheDependancyOnSQLServer/WebForm1.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CacheDependancyOnSQLServer
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+            if (Cache["ProductsData"] != null)
+            {
+                gvProducts.DataSource = Cache["ProductsData"];
+                gvProducts.DataBind();
+
+                lblStatus.Text = "Data retrieved from cache @ " + DateTime.Now.ToString();
+            }
+            else
+            {
+                // Read connection string from web.config file
+                string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                System.Web.Caching.SqlCacheDependencyAdmin.EnableNotifications(CS);
+                System.Web.Caching.SqlCacheDependencyAdmin.EnableTableForNotifications(CS, "Products");
+
+                SqlConnection con = new SqlConnection(CS);
+                SqlDataAdapter da = new SqlDataAdapter("select * from Products", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                SqlCacheDependency sqlDependency = new SqlCacheDependency("Sample", "Products");
+
+                Cache.Insert("ProductsData", ds, sqlDependency);
+
+                gvProducts.DataSource = ds;
+                gvProducts.DataBind();
+                lblStatus.Text = "Data retrieved from database @ " + DateTime.Now.ToString();
+            }
+        }
+    }
+}
