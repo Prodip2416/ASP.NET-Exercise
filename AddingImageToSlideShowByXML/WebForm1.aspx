220b8b5c5843dcc3c301ba4dfe943161ed521917
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-17 02:45:36 +0600

    ASP.NET

---
+++ AddingImageToSlideShowByXML/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="AddingImageToSlideShowByXML.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+           <ContentTemplate>
+               <asp:Timer runat="server" ID="timer" Interval="1000" OnTick="timer_Tick"></asp:Timer>
+            <asp:Image runat="server" ID="image" Height="250px" Width="400px"></asp:Image>
+               <br/>
+               <br/>
+             Name:  <asp:Label ID="Label1" runat="server"> </asp:Label><br/>
+             Order:  <asp:Label ID="Label2" runat="server"></asp:Label>
+               
+               <br/>
+               <asp:Button ID="Button1" runat="server" Text="StopSlideBar" OnClick="Button1_Click" />
+           </ContentTemplate> 
+
+        </asp:UpdatePanel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ AddingImageToSlideShowByXML/WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AddingImageToSlideShowByXML
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadImageData();
+            }
+        }
+        private void LoadImageData()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlDataAdapter da= new SqlDataAdapter("spGetImageData", con);
+            da.SelectCommand.CommandType=CommandType.StoredProcedure;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Images");
+
+            ViewState["ImageData"] = ds;
+
+            ViewState["ImageDisplayed"] = 1;
+            DataRow imageDataRow = ds.Tables["Images"].Select().FirstOrDefault(x => x["order"].ToString() == "1");
+            image.ImageUrl = "~/Images/" + imageDataRow["name"].ToString();
+            Label1.Text = imageDataRow["name"].ToString();
+            Label2.Text = imageDataRow["order"].ToString();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+            {
+                timer.Enabled = false;
+                Button1.Text = "StartSlideBar";
+            }
+            else
+            {
+                timer.Enabled = true;
+                Button1.Text = "StopSlideBar";
+            }
+        }
+
+        protected void timer_Tick(object sender, EventArgs e)
+        {
+            int i = (int)ViewState["ImageDisplayed"];
+            i = i + 1;
+            ViewState["ImageDisplayed"] = i;
+
+            DataRow imageDataRow = ((DataSet)ViewState["ImageData"]).Tables["Images"].Select().FirstOrDefault(x => x["order"].ToString() == i.ToString());
+            if (imageDataRow != null)
+            {
+                image.ImageUrl = "~/Images/" + imageDataRow["name"].ToString();
+                Label1.Text = imageDataRow["name"].ToString();
+                Label2.Text = imageDataRow["order"].ToString();
+            }
+            else
+            {
+                LoadImageData();
+            }
+        }
+    }
+}
