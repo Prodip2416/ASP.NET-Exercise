c5cda6a4eb45bf269724a745613f811e84c61108
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-17 03:26:33 +0600

    ASP.NET

---
+++ BindingMenuControlToXMLFiles/Site.Master.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BindingMenuControlToXMLFiles
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Check(MenuItem item)
+        {
+            if (item.NavigateUrl.Equals(Request.AppRelativeCurrentExecutionFilePath,
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                item.Selected = true;
+            }
+            else if (item.ChildItems.Count > 0)
+            {
+                foreach (MenuItem menuItem in item.ChildItems)
+                {
+                    Check(menuItem);
+                }
+            }
+        }
+
+        protected void Menu1_PreRender(object sender, EventArgs e)
+        {
+            foreach (MenuItem item in Menu1.Items)
+            {
+                Check(item);
+            }
+        }
+    }
+}
---
+++ BindingMenuControlToXMLFiles/home.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="BindingMenuControlToXMLFiles.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    
+</asp:Content>
