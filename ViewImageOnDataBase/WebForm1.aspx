edfa1a00e8459c5aa3d5a33af6c93ff1cbde4ad4
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-17 04:01:07 +0600

    ASP.NET

---
+++ ViewImageOnDataBase/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SaveImageToDataBase.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br/>
+        <br/>
+        <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click" />
+        <br />
+        <br/>
+        <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+        <br/>
+        <br/>
+        <asp:HyperLink ID="HyperLink1" runat="server">View Upload Image</asp:HyperLink>
+        <asp:GridView ID="GridView1" AutoGenerateColumns="False" runat="server">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="Id"/>
+                     <asp:BoundField DataField="Name" HeaderText="Name"/>
+                     <asp:BoundField DataField="Size" HeaderText="Size (bytes)"/>
+                <asp:TemplateField HeaderText="Image">
+                  <ItemTemplate>
+                      <asp:Image ID="Image1" Height="100px" Width="100px"
+                          ImageUrl='<%#"data:Image/png;base64,"+Convert.ToBase64String((byte[])Eval("ImageData")) %>' runat="server" />
+                  </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ViewImageOnDataBase/WebForm1.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SaveImageToDataBase
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblMessage.Visible = false;
+                HyperLink1.Visible = false;
+                LoadImage();
+            }
+        }
+
+        private void LoadImage()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+
+            SqlCommand cmd = new SqlCommand("select * from tblImges", con);
+            con.Open();
+            GridView1.DataSource=cmd.ExecuteReader();
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpPostedFile postedFile = FileUpload1.PostedFile;
+            string fileName = Path.GetFileName(postedFile.FileName);
+            string fileExtension = Path.GetExtension(fileName);
+            int fileSize = postedFile.ContentLength;
+
+            if (fileExtension.ToLower() == ".jpg" || fileExtension.ToLower() == ".bmp"
+                || fileExtension.ToLower() == ".gif" || fileExtension.ToLower() == ".png")
+            {
+                Stream stream = postedFile.InputStream;
+                BinaryReader binary= new BinaryReader(stream);
+                byte[] bytes = binary.ReadBytes((int)stream.Length);
+
+                string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                SqlConnection con= new SqlConnection(CS);
+
+                SqlCommand cmd= new SqlCommand("spUploadImage", con);
+                cmd.CommandType=CommandType.StoredProcedure;
+
+                SqlParameter param= new SqlParameter()
+                {
+                    ParameterName = "@Name",
+                Value= fileName
+                };
+                cmd.Parameters.Add(param);
+                SqlParameter paramSize=new SqlParameter()
+                {
+                    ParameterName = "@Size",
+                    Value = fileSize
+                };
+                cmd.Parameters.Add(paramSize);
+                SqlParameter paramImage= new SqlParameter()
+                {
+                    ParameterName = "@ImageData",
+                    Value = bytes
+                };
+                cmd.Parameters.Add(paramImage);
+                SqlParameter paramNewId= new SqlParameter()
+                {
+                    ParameterName = "@NewId",
+                    Value = -1,
+                    Direction = ParameterDirection.Output
+                };
+                cmd.Parameters.Add(paramNewId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                lblMessage.Visible = true;
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                lblMessage.Text = "Upload Successfully.";
+                HyperLink1.Visible = true;
+                HyperLink1.NavigateUrl = "~/WebForm2.aspx?Id=" + cmd.Parameters["@NewId"].Value.ToString();
+
+                LoadImage();
+            }
+            else
+            {
+                lblMessage.Visible = true;
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Only '.jpg' , '.png' , '.bmp', '.gif' file are uploaded.";
+                HyperLink1.Visible = false;
+            }
+        }
+    }
+}
