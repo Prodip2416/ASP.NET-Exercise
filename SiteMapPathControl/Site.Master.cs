9881803f2fadc5e89ea56625e4c4bba111e04db4
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-17 03:29:09 +0600

    ASP.NET

---
+++ SiteMapPathControl/Site.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SiteMapPathControl
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SiteMapPath1_ItemCreated(object sender, SiteMapNodeItemEventArgs e)
+        {
+            if (e.Item.ItemType == SiteMapNodeItemType.Root ||
+  (e.Item.ItemType == SiteMapNodeItemType.PathSeparator && e.Item.ItemIndex == 1))
+            {
+                e.Item.Visible = false;
+            }
+        }
+    }
+}
---
+++ SiteMapPathControl/home.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="SiteMapPathControl.WebForm1" %>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
