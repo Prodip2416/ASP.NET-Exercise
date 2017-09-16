97a95fc0b3da2cfbdaa4774c8405c83c97b44c05
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-17 02:37:06 +0600

    ASP.NET

---
+++ StopStartSlideShow/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="StopStartSlideShow.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <asp:Timer ID="Timer1" runat="server" Interval="1000" OnTick="Timer1_Tick"></asp:Timer>
+                    <asp:Image ID="Image1" Height="250px" Width="400px" runat="server" />
+                    <br/>
+                    <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+                    <br/>
+                    <br/>
+                    <asp:Button ID="Button1" runat="server" Text="StopSlideBar" OnClick="Button1_Click" />
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StopStartSlideShow/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StopStartSlideShow
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (ViewState["ImageView"] == null)
+            {
+                Image1.ImageUrl = "~/Images/1.jpg";
+                ViewState["ImageView"] = 1;
+                Label1.Text = "Displaying Image 1";
+            }
+            else
+            {
+                int i = (int) ViewState["ImageView"];
+                if (i == 8)
+                {
+
+                    Image1.ImageUrl = "~/Images/1.jpg";
+                    ViewState["ImageView"] = 1;
+                    Label1.Text = "Displaying Image 1";
+                }
+                else
+                {
+                    i = i + 1;
+                    Image1.ImageUrl = "~/Images/" + i.ToString() + ".jpg";
+                    ViewState["ImageView"] = i;
+                    Label1.Text = "Displaying Image " + i.ToString();
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Timer1.Enabled)
+            {
+                Timer1.Enabled = false;
+                Button1.Text = "StartSlideBar";
+            }
+            else
+            {
+                Timer1.Enabled = true;
+                Button1.Text = "StopSlideBar";
+            }
+        }
+    }
+}
