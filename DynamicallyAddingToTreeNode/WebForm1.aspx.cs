f4d1867ff6117db2c9a05a41d212c784bc131c41
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-17 03:43:01 +0600

    ASP.NET

---
+++ DynamicallyAddingToTreeNode/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DynamicallyAddingToTreeNode
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetTreeViewItems();
+            }
+        }
+        private void GetTreeViewItems()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            SqlDataAdapter da = new SqlDataAdapter("spGetTreeViewItems", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            ds.Relations.Add("ChildRows", ds.Tables[0].Columns["ID"],
+                ds.Tables[0].Columns["ParentId"]);
+
+            foreach (DataRow level1DataRow in ds.Tables[0].Rows)
+            {
+                if (string.IsNullOrEmpty(level1DataRow["ParentId"].ToString()))
+                {
+                    TreeNode parentTreeNode = new TreeNode();
+                    parentTreeNode.Text = level1DataRow["TreeViewText"].ToString();
+                    parentTreeNode.NavigateUrl = level1DataRow["NavigateURL"].ToString();
+                    GetChildRows(level1DataRow, parentTreeNode);
+                    TreeView1.Nodes.Add(parentTreeNode);
+                }
+            }
+        }
+        private void GetChildRows(DataRow dataRow, TreeNode treeNode)
+        {
+            DataRow[] childRows = dataRow.GetChildRows("ChildRows");
+            foreach (DataRow row in childRows)
+            {
+                TreeNode childTreeNode = new TreeNode();
+                childTreeNode.Text = row["TreeViewText"].ToString();
+                childTreeNode.NavigateUrl = row["NavigateURL"].ToString();
+                treeNode.ChildNodes.Add(childTreeNode);
+
+                if (row.GetChildRows("ChildRows").Length > 0)
+                {
+                    GetChildRows(row, childTreeNode);
+                }
+            }
+        }
+    }
+}
