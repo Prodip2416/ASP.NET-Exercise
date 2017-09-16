5f6ffe3ecf41148bc3a69ec6e77a2719e5e651ea
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-17 03:57:54 +0600

    ASP.NET

--- SaveImageToDataBase/WebForm2.aspx
+++ SaveImageToDataBase/WebForm2.aspx
@@ -9,7 +9,7 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        <asp:Image ID="Image1" Height="500px" Width="500px" runat="server" />
     </div>
     </form>
 </body>
--- SaveImageToDataBase/WebForm2.aspx.cs
+++ SaveImageToDataBase/WebForm2.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,24 @@ namespace SaveImageToDataBase
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
 
+            SqlCommand cmd = new SqlCommand("spGetImageId", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter param = new SqlParameter()
+            {
+                ParameterName = "@Id",
+                Value = Request.QueryString["Id"]
+            };
+            cmd.Parameters.Add(param);
+            con.Open();
+          
+            byte[] bytes=(byte[]) cmd.ExecuteScalar();
+            string strBase64 = Convert.ToBase64String(bytes);
+            Image1.ImageUrl = "data:Image/png;base64," + strBase64;
+            con.Close();
         }
     }
 }
