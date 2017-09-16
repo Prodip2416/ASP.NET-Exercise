a2baa525cbc9fbb8ebc390b726bf835317f38cf9
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-17 02:42:46 +0600

    ASP.NET

---
+++ UploadAndDownloadFiles/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="UploadAndDownloadFiles.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2" OnRowCommand="GridView1_RowCommand">
+            <Columns>
+                <asp:TemplateField HeaderText="File Name">
+                    <ItemTemplate>
+                        <asp:LinkButton ID="LinkButton1" runat="server" CommandArgument='<%# Eval("File") %>' CommandName="DownLoad" Text='<%# Eval("File") %>'></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="Size" HeaderText="Size In Byte" />
+                <asp:BoundField DataField="Type" HeaderText="File Type" />
+            </Columns>
+            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+            <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#FFF1D4" />
+            <SortedAscendingHeaderStyle BackColor="#B95C30" />
+            <SortedDescendingCellStyle BackColor="#F1E5CE" />
+            <SortedDescendingHeaderStyle BackColor="#93451F" />
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ UploadAndDownloadFiles/WebForm1.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UploadAndDownloadFiles
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Data/")+FileUpload1.FileName);
+            }
+
+            DataTable dt= new DataTable();
+            dt.Columns.Add("File",typeof(string));
+            dt.Columns.Add("Size", typeof(string));
+            dt.Columns.Add("Type", typeof(string));
+
+            foreach (string strFile in Directory.GetFiles(Server.MapPath("~/Data")))
+            {
+                FileInfo fi= new FileInfo(strFile);
+
+                dt.Rows.Add(fi.Name, fi.Length, GetFileTypeByExtension(fi.Extension));
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        private string GetFileTypeByExtension(string fileExtension)
+        {
+            switch (fileExtension.ToLower())
+            {
+                case ".docx":
+                case ".doc":
+                    return "Microsoft Word Document";
+                case ".xlsx":
+                case ".xls":
+                    return "Microsoft Excel Document";
+                case ".txt":
+                    return "Text Document";
+                case ".jpg":
+                case ".png":
+                    return "Image";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            Response.Clear();
+            Response.ContentType = "application/octet-stream";
+            Response.AppendHeader("Content-Disposition","FileName="+e.CommandArgument);
+            Response.TransmitFile(Server.MapPath("~/Data/")+e.CommandArgument);
+            Response.End();
+        }
+    }
+}
