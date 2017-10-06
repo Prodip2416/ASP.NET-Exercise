3d8a35dbaf935209e11d2ba9ead3048fdf14fe5f
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-07 02:06:59 +0600

    ASP.NET

---
+++ ADO.NET/ConnectionStringWebConfig/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConnectionStringWebConfig
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS= ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            SqlConnection con= new SqlConnection(CS);
+            SqlCommand cmd= new SqlCommand("Select * from tblCity", con);
+            con.Open();
+            GridView1.DataSource = cmd.ExecuteReader();
+            GridView1.DataBind();
+            con.Close();
+        }
+    }
+}
