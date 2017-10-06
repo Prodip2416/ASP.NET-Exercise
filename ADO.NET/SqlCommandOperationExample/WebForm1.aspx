104057650acca399a082d3ef5dd2a30d4bb728b1
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-07 02:10:15 +0600

    ASP.NET

---
+++ ADO.NET/SqlCommandOperationExample/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SqlCommandOperationExample.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Vertical">
+            <AlternatingRowStyle BackColor="#DCDCDC" />
+            <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+            <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+            <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#0000A9" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#000065" />
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ADO.NET/SqlCommandOperationExample/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SqlCommandOperationExample
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           string CS= ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+
+            // ExecuteReader ..................
+
+            //SqlCommand cmd= new SqlCommand("Select ID ,DeptName ,Location, DeptHead from tblDepartment", con);
+            //con.Open();
+            //GridView1.DataSource = cmd.ExecuteReader();
+            //GridView1.DataBind();
+            //con.Close();
+
+            //-------------//////---------------------
+
+            // ExecuteScalar....Count Rows.........
+
+            //SqlCommand cmd= new SqlCommand();
+            //cmd.CommandText = "Select count(ID) from tblDepartment";
+            //cmd.Connection = con;
+            //con.Open();
+            //int totalRows = (int) cmd.ExecuteScalar();
+            //con.Close();
+            //Response.Write("Total Rows = "+totalRows.ToString());
+           
+            //------------------///-----------------
+
+            //ExecuteNonQuery...........Insert, Update, Delete....................................
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = "delete from tblDepartment where ID= 7";
+            int totalRows = cmd.ExecuteNonQuery();
+            Response.Write("Total RowsDeleted = " + totalRows.ToString() + "<br/>");
+
+            cmd.CommandText = "Insert into tblDepartment values (11,'dhdtrfhd','Horirampur','Palash')";
+            totalRows = cmd.ExecuteNonQuery();
+            Response.Write("Total Inserted Affected = " + totalRows.ToString() + "<br/>");
+
+            cmd.CommandText = "update tblDepartment set DeptName = 'ttttt' where ID = 3";
+            totalRows = cmd.ExecuteNonQuery();
+            Response.Write("Total Updated Rows Affected = " + totalRows.ToString());
+
+            con.Close();
+
+        }
+    }
+}
