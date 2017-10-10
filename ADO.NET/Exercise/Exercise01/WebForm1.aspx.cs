411b6b83682107fc79a79c2f7afb4d0c46d1bedc
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-10 14:27:36 +0600

    ASP.NET

---
+++ ADO.NET/Exercise/Exercise01/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercise01
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlCommand cmd=new SqlCommand();
+            cmd.CommandText = "Delete tblProduct where id=105";
+            cmd.Connection = con;
+            con.Open();
+           int rows= cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("Rows Affected = "+rows);
+        }
+    }
+}
