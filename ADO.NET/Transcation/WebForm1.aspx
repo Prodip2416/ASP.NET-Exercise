1ebd0171f191470f57230aef4ca0b83df2eb77b8
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-10 14:22:44 +0600

    ASP.NET

---
+++ ADO.NET/Transcation/WebForm1.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Transcation.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: Arial">
+<table border="1" style="background: brown; color: White">
+    <tr>
+        <td>
+            <b>Account Number </b>
+        </td>
+        <td>
+            <asp:Label ID="lblAccountNumber1" runat="server"></asp:Label>
+        </td>
+        <td>
+            <asp:Label ID="lblAccountNumber2" runat="server"></asp:Label>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <b>Customer Name </b>
+        </td>
+        <td>
+            <asp:Label ID="lblName1" runat="server"></asp:Label>
+        </td>
+        <td>
+            <asp:Label ID="lblName2" runat="server"></asp:Label>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <b>Balance </b>
+        </td>
+        <td>
+            <asp:Label ID="lblBalance1" runat="server"></asp:Label>
+        </td>
+        <td>
+            <asp:Label ID="lblBalance2" runat="server"></asp:Label>
+        </td>
+    </tr>
+</table>
+<br />
+<asp:Button ID="btnTransfer" runat="server"
+            Text="Transfer $10 from Account A1 to Account A2"
+            OnClick="btnTransfer_Click" />
+<br />
+<br />
+<asp:Label ID="lblMessage" runat="server" Font-Bold="true"></asp:Label>
+</div>
+    </form>
+</body>
+</html>
---
+++ ADO.NET/Transcation/WebForm1.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Transcation
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetData();
+            }
+        }
+
+        private void GetData()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * From Accounts", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    if (rdr["AccountNumber"].ToString() == "A1")
+                    {
+                        lblAccountNumber1.Text = "A1";
+                        lblBalance1.Text = rdr["Balance"].ToString();
+                        lblName1.Text = rdr["CustomerName"].ToString();
+                    }
+                    else
+                    {
+
+                        lblAccountNumber2.Text = "A2";
+                        lblBalance2.Text = rdr["Balance"].ToString();
+                        lblName2.Text = rdr["CustomerName"].ToString();
+                    }
+                }
+            }
+        }
+
+        protected void btnTransfer_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlTransaction trans = con.BeginTransaction();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(
+                        "Update Accounts set Balance = Balance - 10 where AccountNumber = 'A1'", con, trans);
+                    cmd.ExecuteNonQuery();
+                    cmd = new SqlCommand("Update Accounts set Balance = Balance + 10 where AccountNumber = 'A2'", con, trans);
+                    cmd.ExecuteNonQuery();
+                    trans.Commit();
+                    lblMessage.Text = "Transaction SuccessFully";
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                }
+                catch
+                {
+                    trans.Rollback();
+                    lblMessage.Text = "Transaction SuccessFully";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                }
+  
+            }
+            GetData();
+        }
+    }
+}
