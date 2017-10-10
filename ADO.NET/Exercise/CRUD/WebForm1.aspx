328d68f5a4eff4ea3d84011758595c8bea537d6a
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-10 14:42:31 +0600

    ASP.NET

---
+++ ADO.NET/Exercise/CRUD/WebForm1.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CRUD.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+        <tr>
+            <td>Name:</td>
+            <td>
+                <asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Gender:</td>
+            <td>
+            <asp:DropDownList ID="ddlGender" runat="server">
+                <asp:ListItem Text="Selected Item" Value="-1"></asp:ListItem>
+            <asp:ListItem >Male</asp:ListItem>
+                <asp:ListItem>Female</asp:ListItem>
+            </asp:DropDownList>
+                </td>
+        </tr>
+        <tr>
+            <td>Dept:</td>
+            <td>
+                <asp:TextBox ID="txtDept" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>
+                Campus:
+            </td>
+            <td>
+                <asp:DropDownList ID="ddlCampus" runat="server">
+                     <asp:ListItem Text="Selected Item"  Value="-1"></asp:ListItem>
+                     <asp:ListItem >AB</asp:ListItem>
+                     <asp:ListItem >BC</asp:ListItem>
+                     <asp:ListItem>CA</asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>Delete Id No: <asp:TextBox ID="txtDelete" runat="server"></asp:TextBox></td>
+            <td>
+                <asp:Button ID="btnDelete" runat="server" Text="Delete" Width="103px" OnClick="btnDelete_Click" /></td>
+        </tr>
+        <tr>
+            <td>Update Id No: <asp:TextBox ID="txtUpdate" runat="server"></asp:TextBox></td>
+            <td>
+                <asp:Button ID="btnUpdate" runat="server" Text="Update" Width="102px" OnClick="btnUpdate_Click" /></td>
+        </tr>
+        <tr>
+            <td>ViewWith Id No:<asp:TextBox ID="txtId" runat="server"></asp:TextBox></td>
+            <td>
+                <asp:Button ID="btnId" runat="server" Text="ViewWithId" OnClick="btnId_OnClick" /></td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Button ID="btnInsert" runat="server" Text="Insert" Width="100px" OnClick="btnInsert_Click" /></td>
+            <td>
+               <asp:Button ID="btnViewDataBase" runat="server" Text="View DataBase" OnClick="btnViewDataBase_Click" /></td>
+        </tr>
+        <tr><td>
+            <asp:Label ID="lblMessage" runat="server"></asp:Label></td></tr>
+    </table>
+        <br/>
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Vertical">
+            <AlternatingRowStyle BackColor="#DCDCDC" />
+            <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+            <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+            <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#0000A9" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#000065" />
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ADO.NET/Exercise/CRUD/WebForm1.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void btnViewDataBase_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlCommand cmd= new SqlCommand("Select * from Student",con);
+            con.Open();
+            GridView1.DataSource = cmd.ExecuteReader();
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlCommand cmd=new SqlCommand("spInsertStudent", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Name", txtName.Text);
+            cmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+            cmd.Parameters.AddWithValue("@Dept", txtDept.Text);
+            cmd.Parameters.AddWithValue("@Campus", ddlCampus.SelectedValue);
+
+           SqlParameter outParameter= new SqlParameter();
+            outParameter.ParameterName = "@StudentId";
+            outParameter.SqlDbType = System.Data.SqlDbType.Int;
+            outParameter.Direction = System.Data.ParameterDirection.Output;
+            cmd.Parameters.Add(outParameter);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            String ID = outParameter.Value.ToString();
+            lblMessage.Text = "Student Id " + ID + " Inserted";
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("ssDeleteStudent", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@StudentId ",txtDelete.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                lblMessage.Text = "Delete Successfully.";
+            }
+            catch
+            {
+                lblMessage.Text = "Error";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("spGetUpdateStudents", con);
+
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+                cmd.Parameters.AddWithValue("@Dept", txtDept.Text);
+                cmd.Parameters.AddWithValue("@Campus", ddlCampus.SelectedValue);
+                cmd.Parameters.AddWithValue("@StudentId", txtUpdate.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                lblMessage.Text = "Update Student Sucessfully.";
+            }
+            catch
+            {
+                lblMessage.Text = "Update Not SucessFully.";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+
+        protected void btnId_OnClick(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlDataAdapter da = new SqlDataAdapter("spGetViewtudent", con);
+            con.Open();
+            da.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@StudentId ", txtId.Text);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Students");
+            if (ds.Tables["Students"].Rows.Count > 0)
+            {
+                DataRow dr = ds.Tables["Students"].Rows[0];
+                txtName.Text = dr["Name"].ToString();
+                ddlGender.SelectedValue = dr["Gender"].ToString();
+                txtDept.Text = dr["Dept"].ToString();
+                ddlCampus.SelectedValue = dr["Campus"].ToString();
+            }
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "No Student Record with Id " + txtId.Text;
+            }
+
+
+            con.Close();
+
+
+
+        }
+    }
+}
