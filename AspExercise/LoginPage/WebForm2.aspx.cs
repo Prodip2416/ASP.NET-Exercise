87264a5c895102230329d70b2873790c4fb2b04b
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-14 22:34:20 +0600

    ASP.NET

---
+++ AspExercise/LoginPage/WebForm2.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginPage
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserName"] != null)
+            {
+                lblMessages.ForeColor = System.Drawing.Color.Green;
+                lblMessages.Text = "Wellcome " + Session["UserName"].ToString();
+            }
+        }
+
+        protected void btnSignOut_Click(object sender, EventArgs e)
+        {
+            Session.Remove("UserName");
+            Response.Redirect("~/WebForm1.aspx");
+        }
+    }
+}
